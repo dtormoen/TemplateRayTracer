73bfcf55fa2c17272e84bc9289322ab78d90a5e3
dtormoen <tormo020@umn.edu>
2014-03-25 16:25:25 +0100

    Renamed some things to make template parameters less verbose.

--- src/basicMath/basicMath.cpp
+++ src/basicMath/basicMath.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-constexpr int plusFunc(const int n1, const int n2)
+constexpr int plusOp(const int n1, const int n2)
 { return n1 + n2; }
-constexpr int minusFunc(const int n1, const int n2)
+constexpr int minusOp(const int n1, const int n2)
 { return n1 - n2; }
-constexpr int multFunc(const int n1, const int n2)
+constexpr int multOp(const int n1, const int n2)
 { return n1 * n2; }
-constexpr int divFunc(const int n1, const int n2)
+constexpr int divOp(const int n1, const int n2)
 { return n1 / n2; }
 
 template<int n1, constexpr int(*F)(const int,const int), int n2>
-struct TempOp {
-	static const int result = F(n1, n2);
+struct Op {
+	static const int eq = F(n1, n2);
 };
 
 
 int main(int argc, char const *argv[])
 {
 	cout << "It works!" << endl;
-	cout << "2+2=" << TempOp<2,plusFunc,2>::result << endl;
-	cout << "2-5=" << TempOp<2,minusFunc,5>::result << endl;
-	cout << "3*6=" << TempOp<3,multFunc,6>::result << endl;
-	cout << "10/5=" << TempOp<10,divFunc,5>::result << endl;
+	cout << "2+2=" << Op<2,plusOp,2>::eq << endl;
+	cout << "2-5=" << Op<2,minusOp,5>::eq << endl;
+	cout << "3*6=" << Op<3,multOp,6>::eq << endl;
+	cout << "10/5=" << Op<10,divOp,5>::eq << endl;
 	return 0;
 }
