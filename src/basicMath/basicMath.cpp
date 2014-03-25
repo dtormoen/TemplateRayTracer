babc091512fe23388c7de3ebd3a7f6f2d3b73d0d
dtormoen <tormo020@umn.edu>
2014-03-25 16:19:24 +0100

    Got basic math functions working

--- src/basicMath/basicMath.cpp
+++ src/basicMath/basicMath.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+constexpr int plusFunc(const int n1, const int n2)
+{ return n1 + n2; }
+constexpr int minusFunc(const int n1, const int n2)
+{ return n1 - n2; }
+constexpr int multFunc(const int n1, const int n2)
+{ return n1 * n2; }
+constexpr int divFunc(const int n1, const int n2)
+{ return n1 / n2; }
+
+template<int n1, constexpr int(*F)(const int,const int), int n2>
+struct TempOp {
+	static const int result = F(n1, n2);
+};
+
+
 int main(int argc, char const *argv[])
 {
 	cout << "It works!" << endl;
+	cout << "2+2=" << TempOp<2,plusFunc,2>::result << endl;
+	cout << "2-5=" << TempOp<2,minusFunc,5>::result << endl;
+	cout << "3*6=" << TempOp<3,multFunc,6>::result << endl;
+	cout << "10/5=" << TempOp<10,divFunc,5>::result << endl;
 	return 0;
 }
